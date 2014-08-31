e3ace5e6bb900d9e53804332359824e2453ab9ea
Sebastián Décima <sdecima@gmail.com>
2014-08-31 00:27:12 -0300

    fix para excepciones durante la sincronización

--- src/com/dssd/encuestas/sync/WebSyncAdapter.java
+++ src/com/dssd/encuestas/sync/WebSyncAdapter.java
@@ -10,6 +10,7 @@ import android.content.SyncResult;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.bugsnag.android.Bugsnag;
 import com.dssd.encuestas.datos.AppConfig;
 import com.dssd.encuestas.info.DeviceInfoHelper;
 import com.dssd.encuestas.webservices.EncuestaItem;
@@ -44,40 +45,48 @@ public class WebSyncAdapter extends AbstractThreadedSyncAdapter {
 		
 		String device = AppConfig.getInstance(getContext()).getDevice();
 		if(device != null) {
-			// Sincronizo TiposPreguntas y sus Opciones
-			EncuestasSyncHelper.sincronizarTabla("tipospreguntas", device, getContext(),
-					TiposPreguntasResult.class, TipoPreguntaItem.class);
-			Log.i("WebSyncAdapter", "onPerformSync: sync tipospreguntas");
-			
-			EncuestasSyncHelper.sincronizarTabla("tipospreguntas_opciones", device, getContext(),
-					TiposPreguntasOpcionesResult.class, TipoPreguntaOpcionItem.class);
-			Log.i("WebSyncAdapter", "onPerformSync: sync tipospreguntas_opciones");
-			
-			// Sincronizo los assets (imagenes) de los TipotasOpciones
-			EncuestasSyncHelper.sincronizarAssetsTiposPreguntasOpciones(getContext());
-			Log.i("WebSyncAdapter", "onPerformSync: sync assets preguntas");
-			
-			// Sincronizo Encuestas y Preguntas
-			EncuestasSyncHelper.sincronizarTabla("encuestas", device, getContext(),
-					EncuestasResult.class, EncuestaItem.class);
-			Log.i("WebSyncAdapter", "onPerformSync: sync encuestas");
-			
-			EncuestasSyncHelper.sincronizarTabla("preguntas", device, getContext(),
-					PreguntasResult.class, PreguntaItem.class);
-			Log.i("WebSyncAdapter", "onPerformSync: sync preguntas");
-			
-			EncuestasSyncHelper.sincronizarRespuestas(device, getContext());
-			Log.i("WebSyncAdapter", "onPerformSync: sync respuestas");
-			
-			// Sincronizo los assets (imagenes) de las encuestas (logo)
-			EncuestasSyncHelper.sincronizarAssetsEncuestas(getContext());
-			
-			// Guardo registro
-			float batteryLevel = DeviceInfoHelper.getInstance(getContext()).getBatteryLevel();
-			EncuestasSyncHelper.registroHeartbeat(device, String.format("%2.2f%%", batteryLevel*100), null);
-			
-			if(batteryLevel < 0.3f) {
-				EncuestasSyncHelper.registroBajaBateria(device, String.format("%2.2f%%", batteryLevel*100), null);
+			try {
+				// Sincronizo TiposPreguntas y sus Opciones
+				EncuestasSyncHelper.sincronizarTabla("tipospreguntas", device, getContext(),
+						TiposPreguntasResult.class, TipoPreguntaItem.class);
+				Log.i("WebSyncAdapter", "onPerformSync: sync tipospreguntas");
+				
+				EncuestasSyncHelper.sincronizarTabla("tipospreguntas_opciones", device, getContext(),
+						TiposPreguntasOpcionesResult.class, TipoPreguntaOpcionItem.class);
+				Log.i("WebSyncAdapter", "onPerformSync: sync tipospreguntas_opciones");
+				
+				// Sincronizo los assets (imagenes) de los TipotasOpciones
+				EncuestasSyncHelper.sincronizarAssetsTiposPreguntasOpciones(getContext());
+				Log.i("WebSyncAdapter", "onPerformSync: sync assets preguntas");
+				
+				// Sincronizo Encuestas y Preguntas
+				EncuestasSyncHelper.sincronizarTabla("encuestas", device, getContext(),
+						EncuestasResult.class, EncuestaItem.class);
+				Log.i("WebSyncAdapter", "onPerformSync: sync encuestas");
+				
+				EncuestasSyncHelper.sincronizarTabla("preguntas", device, getContext(),
+						PreguntasResult.class, PreguntaItem.class);
+				Log.i("WebSyncAdapter", "onPerformSync: sync preguntas");
+				
+				EncuestasSyncHelper.sincronizarRespuestas(device, getContext());
+				Log.i("WebSyncAdapter", "onPerformSync: sync respuestas");
+				
+				// Sincronizo los assets (imagenes) de las encuestas (logo)
+				EncuestasSyncHelper.sincronizarAssetsEncuestas(getContext());
+				
+				// Guardo registro
+				float batteryLevel = DeviceInfoHelper.getInstance(getContext()).getBatteryLevel();
+				EncuestasSyncHelper.registroHeartbeat(device, String.format("%2.2f%%", batteryLevel*100), null);
+				
+				if(batteryLevel < 0.3f) {
+					EncuestasSyncHelper.registroBajaBateria(device, String.format("%2.2f%%", batteryLevel*100), null);
+				}
+			} catch (Exception e) {
+				// Si ocurre un error durante la sincronización lo capturo para evitar errores al usuario
+				e.printStackTrace();
+				Log.i("WebSyncAdapter", "onPerformSync: sync error");
+				// Y notifico como "warning" a Bugsnag para investigar
+				Bugsnag.notify(e);
 			}
 		}
 		Log.i("WebSyncAdapter", "onPerformSync: sync ended");
